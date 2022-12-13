ccd652e2eb656bf9c7e74273fa10ea6c522bd785
Lenovo <Lenovo@DESKTOP-75SO5O4>
2022-12-13 01:08:55 +0100

    init

---
+++ SwagLabsProject/SwagLabsProject/Pages/LoginPage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using SwagLabsProject.Driver;
+
+namespace SwagLabsProject.Pages
+{
+    public class LoginPage
+    {
+        private IWebDriver driver = WebDrivers.Instance;
+        public IWebElement UserName => driver.FindElement(By.Id("user-name"));
+        public IWebElement Password => driver.FindElement(By.Id("password"));
+        public IWebElement ButtonLogin => driver.FindElement(By.Id("login-button"));
+        public IWebElement AssertMessage => driver.FindElement(By.CssSelector(".error h3"));
+
+        public void Login(string name, string pass)
+        {
+            UserName.SendKeys(name);
+            Password.SendKeys(pass);
+            ButtonLogin.Submit();
+        }
+    }
+}
---
+++ SwagLabsProject/SwagLabsProject/Tests/LoginTest.cs
@@ -0,0 +1,58 @@
+using SwagLabsProject.Driver;
+using SwagLabsProject.Pages;
+
+namespace SwagLabsProject.Tests
+{
+    public class Tests
+    {
+        LoginPage loginPage;
+        string Message_N01 = "Epic sadface: Username is required";
+        string Message_N02 = "Epic sadface: Username and password do not match any user in this service";
+
+        [SetUp]
+        public void Setup()
+        {
+            WebDrivers.Initialize();
+            loginPage = new LoginPage();
+        }
+        [TearDown]
+        public void Close()
+        {
+            WebDrivers.CleanUp();
+        }
+
+        [Test]
+        public void TC01_DoNotEnterAnyData()
+        {
+            loginPage.Login("", "");
+            Assert.That(Message_N01, Is.EqualTo(loginPage.AssertMessage.Text));
+
+        }
+        [Test]
+        public void TC02_EnterInvalidUserName_AndShouldNotBeLogged()
+        {
+            loginPage.Login("Marko", "secret_sauce");
+            Assert.That(Message_N02,Is.EqualTo(loginPage.AssertMessage.Text));
+
+        }
+        [Test]
+        public void TC03_EnterInvalidPassword_AndShouldNotBeLogged()
+        {
+            loginPage.Login("standard_user", "marko12345");
+            Assert.That(Message_N02, Is.EqualTo(loginPage.AssertMessage.Text));
+
+        }
+        [Test]
+        public void TC04_EnterInvalidUserNameAndPassword_ShouldNotBeLogged()
+        {
+            loginPage.Login("Marko", "Manojlovic");
+            Assert.That(Message_N02, Is.EqualTo(loginPage.AssertMessage.Text));
+        }
+        [Test]
+        public void TC05_EnterValidUserNameAndPassword_ShouldBeLogged()
+        {
+            loginPage.Login("standard_user", "secret_sauce");
+            Assert.That("https://www.saucedemo.com/inventory.html", Is.EqualTo(WebDrivers.Instance.Url));
+        }
+    }
+}

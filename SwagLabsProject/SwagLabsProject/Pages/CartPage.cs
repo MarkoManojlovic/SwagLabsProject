5f9b2dded0bf882d5fcaa2f290de672bbe554a0f
Lenovo <Lenovo@DESKTOP-75SO5O4>
2022-12-13 15:47:18 +0100

    UpdateNewTest

---
+++ SwagLabsProject/SwagLabsProject/Pages/CartPage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using SwagLabsProject.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwagLabsProject.Pages
+{
+    public class CartPage
+    {
+        private IWebDriver driver = WebDrivers.Instance;
+
+        public IWebElement ButtonRemoveBackPack => driver.FindElement(By.Id("remove-sauce-labs-backpack"));
+        public IWebElement ButtonRemoveJacket => driver.FindElement(By.Id("remove-sauce-labs-fleece-jacket"));
+        public IWebElement ContiniueShopping => driver.FindElement(By.Id("continue-shopping"));
+        public IWebElement AssertEmptyBasket => driver.FindElement(By.CssSelector("#shopping_cart_container .shopping_cart_link"));
+    }
+}
